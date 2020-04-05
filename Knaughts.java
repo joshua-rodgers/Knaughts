06f8bb42c38b3908332e2ee280716d18f10475c2
Joshua Rodgers <34038374+joshua-rodgers@users.noreply.github.com>
2020-04-04 20:26:05 -0500

    Update Knaughts.java
    Cleanup. added notes for moving forward. deleted dead code from tests and previous version before restructure.

--- Knaughts.java
+++ Knaughts.java
@@ -23,8 +23,6 @@ public class Knaughts extends KeyAdapter {
         projectiles = new Projectile[10];
         weapons = new Weapon[10];
         
-        
-
         for(int i = 0; i < board.length; i++){
             if(i > 0 && i < 9){
                 for(int j = 0; j < board[i].length; j++){
@@ -40,7 +38,8 @@ public class Knaughts extends KeyAdapter {
                 }
             }
         }
-
+        // NEEDS TO BE CALLED RANDOMLY FOR DIFFERENT WEAPONS 
+        // HARD CODED TO PLACE A RIFLE - JUST A TEST
         place_weapon();
 
         board[player.current.y][player.current.x] = player.avatar;
@@ -53,39 +52,38 @@ public class Knaughts extends KeyAdapter {
         int move = e.getKeyCode();
         switch(move){
             case 32:
-                
+            // HARD CODED TO LOOK AT ZERO
                 if(player.weapon_inventory[0] != null){
-                    System.out.println("space");
                     player.use_weapon();
                 }
                 break;
             case 38:
                 if(player.current.y > 1){
-                    board[player.last.y][player.last.x] = ' ';
+                    board[player.current.y][player.current.x] = ' ';
                     player.current.y -= 1;
-                    board[player.last.y][player.last.x] = player.avatar;
+                    board[player.current.y][player.current.x] = player.avatar;
                 }
                 
                 break;
             case 37:
                 if(player.current.x > 1){
-                    board[player.last.y][player.last.x] = ' ';
+                    board[player.current.y][player.current.x] = ' ';
                     player.current.x -= 1;
-                    board[player.last.y][player.last.x] = player.avatar;
+                    board[player.current.y][player.current.x] = player.avatar;
                 }
                 break;
             case 40:
                 if(player.current.y < board.length - 2){
-                    board[player.last.y][player.last.x] = ' ';
+                    board[player.current.y][player.current.x] = ' ';
                     player.current.y += 1;
-                    board[player.last.y][player.last.x] = player.avatar;
+                    board[player.current.y][player.current.x] = player.avatar;
                 }
                 break;
             case 39:
                 if(player.current.x < board[0].length - 2){
-                    board[player.last.y][player.last.x] = ' ';
+                    board[player.current.y][player.current.x] = ' ';
                     player.current.x += 1;
-                    board[player.last.y][player.last.x] = player.avatar;
+                    board[player.current.y][player.current.x] = player.avatar;
                 }
                 break;
             case 81:
@@ -96,28 +94,14 @@ public class Knaughts extends KeyAdapter {
         }
     }
 
-    public char[][] get_board(){
-        return board;
-    }
-
-    public Projectile[] get_projectiles(){
-        return projectiles;
-    }
-
     public void play(){
         long start_time = System.currentTimeMillis();
         long elapsed = 0;
         long end_time = 0;
 
         while(player.is_alive){
-            
-            if(player.current.x == 1 && player.current.y == 1){
-                player.is_alive = false;
-            }
             for(int i = 0; i < projectiles.length; i++){
                 if(projectiles[i] != null){
-                    
-                    
                     if(projectiles[i].current.y > 0){
                         board[projectiles[i].current.y][projectiles[i].current.x] = ' ';
                         projectiles[i].current.y--;
@@ -131,11 +115,11 @@ public class Knaughts extends KeyAdapter {
                 }
             }
             if(player.current.x == weapons[0].current.x && player.current.y == weapons[0].current.y && weapons[0].is_available == true){
-                
                 player.collect_weapon(weapons[0]);
             }
 
             try {
+                // TRYING TO KEEP GAME LOOP FROM HAPPENING TOO MANY UNNECESSARY TIMES
                 Thread.sleep(125);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -144,12 +128,12 @@ public class Knaughts extends KeyAdapter {
             end_time = System.currentTimeMillis();
             elapsed += end_time - start_time;
 
+            // 6 TIMES PER SECOND SEEMS AS FAST AS CAN BE HANDLED RIGHT NOW
             if(elapsed >= 150){
                 window.surface.render();
                 elapsed = 0;
             }
             start_time = System.currentTimeMillis();
-            
         }
     }
     public static void main(String[] args) {
@@ -158,18 +142,19 @@ public class Knaughts extends KeyAdapter {
 
     class Game_Character {
         Location current;
-        Location last;
+        // Location last; MAYBE FOR FUTURE USE? TELEPORTATION?
         boolean is_alive;
         boolean is_hit;
-        
-    
         Weapon[] weapon_inventory;
     
         char avatar;
     
         public Game_Character(){
             current = new Location();
-            last = new Location();
+            
+            // NEED TO IMPLEMENT PROTECTION AGAINST COLLECTING MO
+            // MORE THAN 3 WEAPONS
+
             weapon_inventory = new Weapon[3];
     
             do{
@@ -180,21 +165,19 @@ public class Knaughts extends KeyAdapter {
                 current.y = r.nextInt(9);
             }while(current.y == 0 || current.y == 9);
     
-            last = current;
             is_alive = true;
             avatar = '@';
         }
     
+        // FOR CHECKING IF A CHARACTER WAS HIT, TO BE CALLED IN PLAY
         public void check_hit(){
     
         }
     
         public void use_weapon(){
-            Projectile created = new Projectile();
             for(int i = 0; i < projectiles.length; i++){
-                
                 if(projectiles[i] == null){
-                    projectiles[i] = created;
+                    projectiles[i] = new Projectile();
                     projectiles[i].current.x = player.current.x;
                     projectiles[i].current.y = player.current.y - 1;
                     board[projectiles[i].current.y][projectiles[i].current.x] = projectiles[i].sprite;
@@ -216,17 +199,14 @@ public class Knaughts extends KeyAdapter {
     
     }
 
-    public interface Item {
 
-    }
-
-    abstract class Weapon implements Item {
+    abstract class Weapon{
         char type;
         int power;
         boolean is_available;
     
         Location current;
-        Location last;
+        // Location last; MAYBE FOR FUTURE USE? TELEPORTATION?
 
         public char get_type(){
             return type;
@@ -246,11 +226,11 @@ public class Knaughts extends KeyAdapter {
 
             do{
                 current.x = r.nextInt(15);
-            }while(current.x == 0 || current.x == 15);
+            }while(current.x == 0 || current.x == 15); // HARD CODED TO 15 WIDE
             
             do{
                 current.y = r.nextInt(9);
-            }while(current.y == 0 || current.y == 9);
+            }while(current.y == 0 || current.y == 9); // HARD CODED TO 9 HIGH
 
             ammo = new char[5];
             for(int i = 0; i < ammo.length; i++){
@@ -264,7 +244,7 @@ public class Knaughts extends KeyAdapter {
     }
 
     private void place_weapon(){
-        
+        // HARD CODED TO PLACE RIFLE, NEED TO BE GENERAL 
         weapons[0] = new Rifle();
 
         board[weapons[0].current.y][weapons[0].current.x] = weapons[0].get_type();
@@ -273,11 +253,11 @@ public class Knaughts extends KeyAdapter {
     
     class Projectile {
         Location current;
-        Location last;
+        // Location last; MAYBE FOR FUTURE USE? TELEPORTATION?
         char sprite;
+        int power;
 
         public Projectile(){
-            current = new Location();
             current = new Location();
             sprite = '\'';
         }
@@ -306,11 +286,9 @@ class Game_Surface extends Panel{
     Image bbuffer;
     char[][] board;
     
-
     public Game_Surface(Knaughts state, int width, int height){
         this.state = state;
         this.setSize(width, height);
-        
         this.setFont(new Font("Monospaced", 1, 20));
     }
 
@@ -320,14 +298,13 @@ class Game_Surface extends Panel{
         bcontext = bbuffer.getGraphics();
         render();
     }
-
+    // NEED TO RESEARCH POSSIBLE WAYS TO BE MORE EFFICIENT
     public void render(){
-        //board = state.get_board();
-
         int ypos = 40;
         String curr_row = "";
         bcontext.clearRect(0, 0, 300, 300);
 
+        // THIS SEEMS LIKE A PROBLEM AREA, COULD BE FASTER?
         for(char[] inner : state.board){
             for(char c : inner){
                 curr_row += c;
@@ -338,12 +315,10 @@ class Game_Surface extends Panel{
         }
         context.drawImage(bbuffer, 0, 0, this);
         
-        //context.dispose();
+        //context.dispose(); DONT KNOW IF THIS HELPS OR HURTS
     }
 }
-
-
-
+// UTILIY CLASS FOR SEMANTICS
 class Location {
     int x;
     int y;
